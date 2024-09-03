0ab3a72f4199d96df654a86c0a44224c17b49af1
Sarathi Prabu <sarathiprabum@gmail.com>
2024-09-03 01:16:07 -0400

    Create backend

---
+++ app/api/users/route.ts
@@ -0,0 +1,19 @@
+
+export function GET(){
+    return Response.json({
+        name: "Sarathi Prabu GET",
+        email: "sarathiprabum@gmail.com"
+    })
+}
+export function POST(){
+    return Response.json({
+        name: "Sarathi Prabu POST",
+        email: "sarathiprabum@gmail.com"
+    })
+}
+export function PUT(){
+    return Response.json({
+        name: "Sarathi Prabu PUT",
+        email: "sarathiprabum@gmail.com"
+    })
+}
