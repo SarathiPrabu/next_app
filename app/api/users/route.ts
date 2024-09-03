d52d752db5a6637fb9ab9f89e745964d9cd1fa68
Sarathi Prabu <sarathiprabum@gmail.com>
2024-09-03 12:53:25 -0400

    Signup - Test Get and Post requests

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,22 +1,26 @@
-import { NextRequest } from "next/server"
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server"
 
-export function GET(){
-    return Response.json({
-        name: "Sarathi Prabu GET",
-        email: "sarathiprabum@gmail.com"
-    })
+const client = new PrismaClient();
+
+export async function GET() {
+    const user = await client.user.findFirst({});
+    return Response.json({ name: user?.username, email: user?.id })
 }
-export async function POST(req: NextRequest){
+
+export async function POST(req: NextRequest) {
     const body = await req.json();
-    console.log(body)
+    // should add zod validation here
+    const user = await client.user.create({
+        data: {
+            username: body.username,
+            password: body.password
+        }
+    });
 
-    return Response.json({
-        message: "You are loggin in Successfully!!"
-    })
+    return NextResponse.json({ 
+        username: body.username,
+        message:"Signup success"
+     });
 }
-export function PUT(){
-    return Response.json({
-        name: "Sarathi Prabu PUT",
-        email: "sarathiprabum@gmail.com"
-    })
-}
+
